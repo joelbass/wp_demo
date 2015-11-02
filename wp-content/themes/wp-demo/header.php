368d1b58002c806f22725ec5acc5398a27813459
joelbass <joelbass@gmail.com>
2015-11-02 13:01:17 -0700

    scss

--- wp-content/themes/wp-demo/header.php
+++ wp-content/themes/wp-demo/header.php
@@ -27,12 +27,9 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="row">
 			<div class="site-branding">
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php endif; ?>
-			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+		    <img src="<?php bloginfo('template_directory'); ?>/images/logo-header.svg" alt="Your Image Description Here" />
+		</a>
 		</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
